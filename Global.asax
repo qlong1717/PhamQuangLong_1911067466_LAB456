f07c7fe2c82adb89a634ab73eb3ef3a5f2cbd6b8
Qlong <Qlong@PQLOG>
2022-05-30 10:59:06 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhamQuangLong_1911067466_LAB456.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhamQuangLong_1911067466_LAB456.Startup))]
+namespace PhamQuangLong_1911067466_LAB456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
